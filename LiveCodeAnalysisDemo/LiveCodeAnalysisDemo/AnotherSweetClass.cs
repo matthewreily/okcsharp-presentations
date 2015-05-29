33539de62900c270e282466f2681ff8ba2d1ec04
mreily <matthew.reily@interworks.com>
2015-05-29 17:27:03 -0500

    initial commit

---
+++ LiveCodeAnalysisDemo/LiveCodeAnalysisDemo/AnotherSweetClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveCodeAnalysisDemo
+{
+    public class AnotherSweetClass
+    {
+        /// <summary>
+        /// SweetClass property
+        /// </summary>
+        public SweetClass1 MyClass { get; set; }
+    }
+}
---
+++ LiveCodeAnalysisDemo/LiveCodeAnalysisDemo/SweetClass.cs
@@ -0,0 +1,33 @@
+﻿//Show removing unused usings
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace LiveCodeAnalysisDemo
+{
+    //Show rename
+    public class SweetClass1
+    {
+        
+        public String MyString { get; set; }
+        public string anotherString { get;set; }
+
+        public void DoWork(params int[] args)
+        {
+            //Show Introduce local
+            var newNumber = Convert.ToDouble(args[0]) + Convert.ToDouble(args[0]);
+
+            //show in-line temporary variable
+            var evenNewerNumber = newNumber * Convert.ToDouble(args[0]);
+
+            //Show rename
+            var icntType = 100;
+            for (int i = 0; i < icntType; i++)
+            {
+                Write(i);
+            }
+        }
+    }
+}
