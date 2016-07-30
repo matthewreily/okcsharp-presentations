384d9c4e4187c64a2c6d08d46fe68a852b98185f
Matthew Reily <matthew.reily@interworks.com>
2016-07-30 14:07:56 -0500

    first attempt at pace calculator demo. Created console app and corresponding test project

---
+++ PaceCalculator/PaceCalculator.Tests/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using PaceCalculator;
+using Xunit;
+namespace ConsoleApplication
+{
+    public class PaceCalculatorTests
+    {
+        [Fact]
+        public void CanGetTimespanForValidEntry()
+        {
+           var time = "03:00:00";
+           var timeSpan = PaceCalculator.App.GetTime(time); 
+        }
+    }
+}
---
+++ PaceCalculator/PaceCalculator/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace PaceCalculator
+{
+    public class App
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Pace Calculator! Press ENTER to begin");
+            Console.ReadLine();
+
+            Console.WriteLine("Would you like to use miles(m) or kilometers(km)?");
+            var distanceType = GetDistanceType(Console.ReadLine());
+            
+            Console.WriteLine("What distance would you like to use for the calculation?");
+            var distance = Console.ReadLine();
+            
+            Console.WriteLine("What is your desired finishing time?\nUse hh:mm.ss format. ex 01:10.00 for 1 hour 10 minutes");
+            var time = GetTime(Console.ReadLine());
+            
+            var pace = RequiredPaceCalculator(time, GetDistance(distance));
+            Console.WriteLine($"Your required pace to run {distance} {distanceType} is {pace.ToString()} per {distanceType}");
+        }
+
+        public static string GetDistanceType(string distanceType)
+        {
+            if(distanceType.StartsWith("m", StringComparison.OrdinalIgnoreCase) || distanceType.StartsWith("k",StringComparison.OrdinalIgnoreCase))
+            {
+                return distanceType.ToLower()[0] == 'm' ? "mile(s)" : "kilometer(s)";
+            }
+            throw new ArgumentException("Expected either m or k for miles or kilometers", "distanceType");
+        }
+        public static double GetDistance(string distance)
+        {
+            double distanceFromString;
+            if(Double.TryParse(distance, out distanceFromString)){
+                return distanceFromString;
+            }
+            throw new ArgumentException("Distance provided was not a valid double.","distance");
+        }
+
+        public static TimeSpan RequiredPaceCalculator(TimeSpan time, double distance)
+        {
+            double pace = 0;
+            
+            var totalSeconds = time.TotalSeconds;
+            
+            pace = (double) totalSeconds / distance;
+
+            return TimeSpan.FromSeconds(pace);
+
+        }
+        public static TimeSpan GetTime(string time)
+        {
+            TimeSpan timeParsed;
+            if(TimeSpan.TryParse(time, out timeParsed))
+            {
+                return timeParsed;
+            }
+            throw new ArgumentException("Time was not in required format (hh:mm.ss).", "time");
+        }
+    }
+}
